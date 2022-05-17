eb752418f7755e9ccf105143cb8b08036be7e1ea
Ben England <benengland82@gmail.com>
2022-05-17 08:02:49 -0400

    actually use test_readwav generated file remove test artifacts when done abort test if any errors observed by shell

--- test_readwav.sh
+++ test_readwav.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -eEo
+
 # script to exercise readwav.c 
-wav_file=short.wav
+wav_file=${1:-short.wav}
 suffix="_test_write"
+new_file="$(basename $wav_file .wav)${suffix}.wav"
+newer_file="$(basename $new_file .wav)${suffix}.wav"
+rm -f $new_file $newer_file
 
 export WAV_DEBUG=1 
 echo original wav file
@@ -9,14 +14,13 @@ pacat $wav_file
 sleep 1
 
 ./test_readwav $wav_file
-new_file="$(basename $wav_file .wav)${suffix}.wav"
 echo copied to $new_file
 pacat $new_file
 sleep 1
 
 # verify what we write is playable and parsable
 
-new_file="$(basename $new_file .wav)${suffix}.wav"
-echo copied to $new_file
-pacat $new_file
-sleep 1
+./test_readwav $new_file
+echo copied $new_file to $newer_file
+pacat $newer_file
+rm -f $new_file $newer_file
