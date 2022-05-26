64e4b896e1eb8267a0f25b850e1bdd4526bf147d
Ben England <benengland82@gmail.com>
2022-05-26 16:30:44 -0400

    user specifies output file path in param 2 program doesn't generate it from input file path

--- copy_wav_file.c
+++ copy_wav_file.c
@@ -1,30 +1,23 @@
+/* copy .wav file from specified source to destination */
+
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
 #include "wav_file_access.h"
 
-static char out_path[1024];
 
 int main(int argc, char **argv) {
 	int rc;
 	wav_sample_t * sample_buf;
 	int sample_count;
         int chans;
-	char *dot;
 
 	rc = wav_read(argv[1], &sample_buf, &sample_count, &chans);
-	assert(rc == 0);
+	if (rc) return rc;
 	printf("sample count %d, channels %d\n", sample_count, chans);
 	/* print_samples(sample_buf, sample_count); */
 
-	strcpy(out_path, argv[1]);
-	dot = rindex(out_path, '.');
-	if (strcmp(dot, ".wav")) {
-		printf("input file must end in .wav\n");
-		exit(1);
-	}
-	strcpy(dot, "_test_write.wav");
-	rc = wav_write(out_path, sample_buf, sample_count, chans);
-	assert(rc == 0);
+	rc = wav_write(argv[2], sample_buf, sample_count, chans);
+	return rc;
 }
