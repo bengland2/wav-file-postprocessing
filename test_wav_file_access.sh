a32ca2ba4e9cc79e77d24ec995b066e6df202979
Ben England <benengland82@gmail.com>
2022-06-09 17:21:21 -0400

    oops need 2nd pathname

--- test_wav_file_access.sh
+++ test_wav_file_access.sh
@@ -6,21 +6,36 @@ wav_file=${1:-short.wav}
 suffix="_test_write"
 new_file="$(basename $wav_file .wav)${suffix}.wav"
 newer_file="$(basename $new_file .wav)${suffix}.wav"
-rm -f $new_file $newer_file
+xform_file="$(basename $new_file .wav)_xform.wav"
+
+function cleanup()
+{
+rm -f $new_file $newer_file $xform_file
+}
+
+cleanup
 
 export WAV_DEBUG=1 
 echo original wav file
 pacat $wav_file
 sleep 1
 
-./copy_wav_file $wav_file
+./copy_wav_file $wav_file $new_file
 echo copied to $new_file
 pacat $new_file
 sleep 1
 
 # verify what we write is playable and parsable
 
-./copy_wav_file $new_file
+./copy_wav_file $new_file $newer_file
 echo copied $new_file to $newer_file
 pacat $newer_file
-rm -f $new_file $newer_file
+
+# transform sound of this file
+
+./wav_transform -a 0.5 -m 100 -f 4000 -l -1.0 $wav_file $xform_file
+echo transformed $wav_file to $xform_file
+pacat $xform_file
+
+
+
